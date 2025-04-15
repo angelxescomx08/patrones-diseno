c2209482856d98a9c618d8bfb03f5216a56c3a2c
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2025-04-15 08:39:12 -0600

    feat: abstract factory and factory method

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,62 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Hamburger {
+  prepare(): void;
+}
+
+class ChickenHamburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparing %cChicken Hamburger", COLORS.brown);
+  }
+}
+
+class BeefHamburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparing %cBeef Hamburger",COLORS.brown);
+  }
+}
+
+abstract class Restaurant {
+  abstract createHamburger(): Hamburger;
+
+  orderHamburger(): void {
+    const hamburger = this.createHamburger();
+    hamburger.prepare();
+  }
+}
+
+class ChickenRestaurant extends Restaurant {
+  createHamburger(): Hamburger {
+    return new ChickenHamburger();
+  }
+}
+
+class BeefRestaurant extends Restaurant {
+  createHamburger(): Hamburger {
+    return new BeefHamburger();
+  }
+}
+
+function main() {
+  let restaurant: Restaurant;
+  const burguerType = prompt("¿Qué tipo de hamburguesas desea? (chicken/beef)");
+
+  switch (burguerType) {
+    case "chicken":
+      restaurant = new ChickenRestaurant();
+      break;
+    case "beef":
+      restaurant = new BeefRestaurant();
+      break;
+    default:
+      throw new Error("Invalid burguer type");
+  }
+
+  restaurant.orderHamburger();
+}
+
+main();
--- 01-creacionales/03-abstract-factory.ts
+++ 01-creacionales/03-abstract-factory.ts
@@ -12,8 +12,74 @@
  * https://refactoring.guru/es/design-patterns/abstract-factory
  */
 
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  *  El propósito del Abstract Factory es crear familias de objetos relacionados
  *  (en este caso, hamburguesas y bebidas) sin especificar las clases concretas
  *  de cada uno de esos objetos en el código principal.
  */
+
+interface Hamburger {
+  prepare(): void;
+}
+
+interface Drink{
+  pour(): void;
+}
+
+class ChickenHamburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparing %cChicken %cHamburger", COLORS.brown);
+  }
+}
+
+class BeefHamburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparing %cBeef %cHamburger", COLORS.brown);
+  }
+}
+
+class Water implements Drink {
+  pour(): void {
+    console.log("Pouring %cWater", COLORS.blue);
+  }
+}
+
+class Soda implements Drink {
+  pour(): void {
+    console.log("Pouring %cSoda", COLORS.brown);
+  }
+}
+
+interface RestaurantFactory {
+  createHamburger(): Hamburger;
+  createDrink(): Drink;
+}
+
+class FastFoodRestaurantFactory implements RestaurantFactory {
+  createHamburger(): Hamburger {
+    return new BeefHamburger();
+  }
+
+  createDrink(): Drink {
+    return new Soda();
+  }
+}
+
+class HealtyFoodRestaurantFactory implements RestaurantFactory {
+  createHamburger(): Hamburger {
+    return new ChickenHamburger();
+  }
+
+  createDrink(): Drink {
+    return new Water();
+  }
+}
+
+function main(factory: RestaurantFactory) {
+  factory.createHamburger().prepare();
+  factory.createDrink().pour();
+}
+
+main(new FastFoodRestaurantFactory());
