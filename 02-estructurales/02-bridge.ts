5e1b7d312134e2dd8f512df4c2e17419c96c2250
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2025-07-03 08:23:42 -0600

    Add Bridge pattern implementation with Character classes and abilities

--- 02-estructurales/02-bridge.ts
+++ 02-estructurales/02-bridge.ts
@@ -9,3 +9,68 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Ability{
+  use(): void;
+}
+
+class SwordAttack implements Ability {
+  use(): void {
+    console.log("Using %csword attack", COLORS.blue);
+  }
+}
+
+class MagicSpell implements Ability {
+  use(): void {
+    console.log("Using %cmagic spell", COLORS.green);
+  }
+}
+
+class AxeAttack implements Ability {
+  use(): void {
+    console.log("Using %caxe attack", COLORS.red);
+  }
+}
+
+abstract class Character {
+  protected ability: Ability;
+
+  constructor(ability: Ability) {
+    this.ability = ability;
+  }
+
+  setAbility(ability: Ability) {
+    this.ability = ability;
+  }
+
+  abstract performAbility(): void;
+}
+
+class Warrior extends Character {
+  override performAbility(): void {
+    console.log("Warrior is performing ability");
+    this.ability.use();
+  }
+}
+
+class Mage extends Character {
+  override performAbility(): void {
+    console.log("Mage is performing ability");
+    this.ability.use();
+  }
+}
+
+function main() {
+  const warrior = new Warrior(new SwordAttack());
+  warrior.performAbility();
+
+  const mage = new Mage(new MagicSpell());
+  mage.performAbility();
+
+  warrior.setAbility(new AxeAttack());
+  warrior.performAbility();
+}
+
+main();
