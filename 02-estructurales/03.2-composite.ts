2c16f5e0e9258edced0f5a7181f8db3ae638e871
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2025-07-04 08:51:32 -0600

    Enhance MenuCategory class in Composite pattern by adding private properties for name and items, and implementing method overloading for add function to handle both single and multiple MenuComponent items.

--- 02-estructurales/03.2-composite.ts
+++ 02-estructurales/03.2-composite.ts
@@ -47,16 +47,31 @@ class MenuCategory implements MenuComponent {
   // TODO: Crear dos propiedades privadas: name y items
   // Name sting y items arreglo de MenuComponent
   // Name es recibida en el constructor, items se inicializa como un arreglo vacío
+  private name: string;
+  private items: MenuComponent[] = [];
+
+  constructor(name: string){
+    this.name = name;
+  }
 
   //TODO: Sobrecarga de operadores - Item puede ser MenuComponent o un arreglo de MenuComponent
-  add(item: unknown): void {
+  add(item: MenuComponent | MenuComponent[]): void {
     // TODO: Implementar la sobrecarga de operadores
-    throw new Error('Method not implemented.');
+    if(item instanceof Array){
+      item.forEach(i=>{
+        this.items.push(i);
+      })
+      return;
+    }
+    this.items.push(item);
   }
 
   showDetails(indent: string = ''): void {
     console.log(`%c${indent}+ ${this.name}`, COLORS.blue);
     // TODO: Implementar foreach
+    this.items.forEach(item => {
+      item.showDetails(indent);
+    })
   }
 }
 
