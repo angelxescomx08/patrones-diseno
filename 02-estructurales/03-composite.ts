868bd3c0c0f6426847b87de02cc7df12cd5752bb
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2025-07-04 08:38:45 -0600

    Add Composite pattern implementation with File and Folder classes, enabling hierarchical structure representation and detail display functionality.

--- 02-estructurales/03-composite.ts
+++ 02-estructurales/03-composite.ts
@@ -13,3 +13,63 @@
  * https://refactoring.guru/es/design-patterns/composite
  *
  */
+
+interface FileSystemComponent {
+  showDetails(indent?:string): void;
+}
+
+class File implements FileSystemComponent{
+  private name: string;
+
+  constructor(name:string){
+    this.name = name;
+  }
+
+  showDetails(indent: string = ""): void {
+    console.log(`${indent}- Archivo: ${this.name}`);
+  }
+  
+}
+
+class Folder implements FileSystemComponent{
+  private name: string;
+  private components: FileSystemComponent[] = [];
+
+  constructor(name:string){
+    this.name = name;
+  }
+
+  add(component: FileSystemComponent){
+    this.components.push(component);
+  }
+
+  showDetails(indent: string = ""): void {
+    console.log(`${indent}- Carpeta: ${this.name}`);
+    this.components.forEach(component=>{
+      component.showDetails(`${indent} `);
+    })
+  }
+}
+
+function main(){
+  const file1 = new File("Archivo1.txt");
+  const file2 = new File("Archivo2.txt");
+  const file3 = new File("Archivo3.txt");
+  const file4 = new File("Archivo4.txt");
+
+  const folder1 = new Folder("Carpeta1");
+  folder1.add(file1);
+  folder1.add(file2);
+
+  const folder2 = new Folder("Carpeta2");
+  folder2.add(file3);
+  folder2.add(file4);
+
+  const folder3 = new Folder("Carpeta3");
+  folder3.add(folder1);
+  folder3.add(folder2);
+
+  folder3.showDetails();
+}
+
+main()
