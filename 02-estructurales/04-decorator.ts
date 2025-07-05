f6cf11b893cf80527b9c0e9775ed6e37993ed3b2
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2025-07-04 19:00:12 -0600

    feat: add decorator pattern

--- 02-estructurales/04-decorator.ts
+++ 02-estructurales/04-decorator.ts
@@ -11,3 +11,59 @@
  *
  * https://refactoring.guru/es/design-patterns/decorator
  */
+
+interface Notification {
+  send(message: string): void;
+}
+
+class BasicNotification implements Notification {
+  send(message: string): void {
+    console.log(`Sending basic notification: ${message}`);
+  }
+}
+
+abstract class NotificationDecorator implements Notification {
+  protected notification: Notification;
+
+  constructor(notification: Notification) {
+    this.notification = notification;
+  }
+
+  send(message: string): void {
+    this.notification.send(message);
+  }
+}
+
+class EmailDecorator extends NotificationDecorator {
+
+  private sendEmail(message: string): void {
+    console.log(`Sending email notification: ${message}`);
+  }
+  
+  override send(message: string): void {
+    super.send(message);
+    this.sendEmail(message);
+  }
+}
+
+class SMSDecorator extends NotificationDecorator {
+
+  private sendSMS(message: string): void {
+    console.log(`Sending SMS notification: ${message}`);
+  }
+  
+  override send(message: string): void {
+    super.send(message);
+    this.sendSMS(message);
+  }
+}
+
+function main() {
+  const notification = new BasicNotification();
+  const emailNotification = new EmailDecorator(notification);
+  const smsNotification = new SMSDecorator(emailNotification);
+
+  smsNotification.send('Hello, world!');
+}
+
+main();
