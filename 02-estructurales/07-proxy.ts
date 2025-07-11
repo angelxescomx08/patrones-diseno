fe596e6fb7c6f2b91d10a54154aef2f4371a4ec1
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2025-07-11 16:41:58 -0600

    feat: implement Proxy pattern with MagicPortal and SecretRoom classes, controlling access based on player level

--- 02-estructurales/07-proxy.ts
+++ 02-estructurales/07-proxy.ts
@@ -10,3 +10,54 @@
  * https://refactoring.guru/es/design-patterns/proxy
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Player{
+  name: string;
+  level: number;
+
+  constructor(name: string, level: number){
+    this.name = name;
+    this.level = level;
+  }
+}
+
+interface Room{
+  enter(player: Player):void;
+}
+
+class SecretRoom implements Room{
+  enter(player: Player): void {
+    console.log(`%cBienvenido a la sala secreta ${player.name}`,COLORS.green)
+  }
+}
+
+//Clase del proxy
+class MagicPortal implements Room{
+  private secretRoom: Room;
+
+  constructor(room: Room){
+    this.secretRoom = room;
+  }
+
+  enter(player: Player): void {
+    if(player.level >= 10){
+      this.secretRoom.enter(player);
+      return;
+    }
+
+    console.log(`%cEres de nivel ${player.level} debes ser nivel superior a 10`,COLORS.red)
+  }
+}
+
+function main(){
+  const portal = new MagicPortal(new SecretRoom());
+  const player1 = new Player("Aventurero A", 5);
+  const player2 = new Player("Aventurero b", 10);
+
+  portal.enter(player1);
+  portal.enter(player2);
+}
+
+main()
